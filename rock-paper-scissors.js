c36b12bf8bae6a62016bcec1f95c9ff9bfc50eb9
jflann <26899145+jflann@users.noreply.github.com>
2022-12-11 19:43:57 -0600

    Improve UI

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,106 +5,113 @@ function getRandomInt(min, max) {
 }
 
 function getComputerChoice() {
-  let choice;
   switch (getRandomInt(0,2)) {
     case 0:
-      choice = 'rock';
-      break
+      return 'rock';
     case 1:
-      choice = 'paper';
-      break
+      return 'paper';
     case 2:
-      choice = 'scissors';
-      break
+      return 'scissors';
   }
-  return choice;
 }
 
-function getPlayerChoice() {
-  let choice = {
-    value: null,
-    valid: false
+function referee() {
+  let training = {};
+  function learn(winner, loser) {
+    if (!training[winner]) training[winner] = loser;
   }
-  while (choice.valid == false) {
-    choice.value = prompt('Rock, paper, or scissors?');
-    if (['rock', 'paper', 'scissors'].includes(choice.value.toLowerCase())) {
-      choice.valid = true;
-    }
+  function judge(choiceOne, choiceTwo) {
+    // returns winner as 'choiceOne' or 'choiceTwo', or returns 'tie'
+    if (choiceOne === choiceTwo) return 'tie';
+    return (training[choiceOne] === choiceTwo ? 'choiceOne' : 'choiceTwo');
   }
-  return choice.value;
+  function validate(choice) {
+    return choice in training;
+  }
+  function choices() {
+    return Object.keys(training);
+  }
+  return {
+    'learn': learn,
+    'playRound': judge,
+    'validAction': validate,
+    'getChoices': choices
+  }
 }
 
-function playRound(playerChoice, computerChoice) {
+let playerScore = 0;
+let computerScore = 0;
+let gameState = 'start';
+let gameMessage = 'Make your selection!';
 
-  playerChoice = playerChoice.toLowerCase();
-  
-  // determine result
-  let result;
-  switch (playerChoice + computerChoice) {
-    // ties
-    case 'rockrock':
-    case 'scissorsscissors':
-    case 'paperpaper':
-      result = 'tie';
-      alert("It's a tie!");
-      break
+let ref = referee();
+ref.learn('rock', 'scissors');
+ref.learn('paper', 'rock');
+ref.learn('scissors', 'paper');
 
-    // losses
-    case 'rockpaper':
-    case 'paperscissors':
-    case 'scissorsrock':
-      result = 'lose';
-      alert(`You lose! ${computerChoice} beats your ${playerChoice}.`);
-      break
-    
-    // wins
-    case 'rockscissors':
-    case 'paperrock':
-    case 'scissorspaper':
-      result = 'win';
-      alert(`You win! Your ${playerChoice} beats ${computerChoice}.`)
-      break
-  }
+const startButton = document.querySelector('#start-game');
+startButton.addEventListener('click', startGame);
 
-  return result;
+startGame();
+
+function startGame() {
+  playerScore = 0;
+  computerScore = 0;
+  // clear score bars
+  const scoreBars = document.querySelectorAll('.score-bar');
+  scoreBars.forEach(div => div.replaceChildren());
+
+  const choiceButtons = document.querySelectorAll(".game-button");
+  choiceButtons.forEach(choiceButton => choiceButton.addEventListener("click", choose));
 }
 
-function game(rounds = 5) {
-
-  let playerWins = 0;
-  let computerWins = 0;
-  let roundTies = 0;
-
-  // play rounds
-  for(let i = 0; i < rounds; i++) {
-
-    roundResult = playRound(getPlayerChoice(), getComputerChoice())
-
-    switch (roundResult) {
-      case 'win':
-        playerWins++;
-        break
-      case 'lose':
-        computerWins++;
-        break
-      case 'tie':
-        roundTies++;
-    }
-  }
-
-  // get game result
-  let gameResult;
-  if (playerWins > computerWins) {
-    gameResult = `Player wins! ${playerWins} : ${computerWins}`;
-  }
-  else if (playerWins < computerWins) {
-    gameResult = `Computer wins! ${playerWins} : ${computerWins}`;
-  }
-  else if (playerWins == computerWins) {
-    gameResult = `It's a tie! ${playerWins} : ${computerWins}`;
-  }
-
-  alert(gameResult);
-  return gameResult
-  
+function endGame(winner) {
+  alert(`${winner} wins!`);
 }
+
+function choose() {
+  console.log(this);
+  switch (ref.playRound(this.id, getComputerChoice())) {
+    case 'choiceOne':
+      handleRound('playerWin');
+      return
+    case 'choiceTwo':
+      handleRound('computerWin');
+      return
+    case 'tie':
+      handleRound('tie')
+      return
+  }
+}
+
+function handleRound(outcome) {
+  if (outcome === 'tie') {
+    gameMessage = "It's a tie!";
+  }
+  if (outcome === 'playerWin') {
+    incrementScore('player');
+  }
+  if (outcome === 'computerWin') {
+    incrementScore('computer');
+  }
+
+  if (playerScore === 5) endGame('player');
+  if (computerScore === 5) endGame('computer');
+}
+
+function incrementScore(player) {
+  if (player === 'player') {
+    barId = 'player-score-bar';
+    playerScore++;
+  }
+  if (player === 'computer') {
+    barId = 'computer-score-bar';
+    computerScore++;
+  }
+
+  const scoreBar = document.querySelector(`#${barId}`);
+  const point = document.createElement('div');
+  point.classList.add('point');
+  scoreBar.appendChild(point);
+}
+
